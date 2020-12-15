080d814e2ff6a77142742096b1abe4f1c47f0a33
uu64 <yusuke.t.0808@gmail.com>
2020-12-15 23:43:57 +0900

    add: survey (plugin for interactive cli)

--- main.go
+++ main.go
@@ -1,24 +1,12 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/manifoldco/promptui"
-)
+import "github.com/AlecAivazis/survey"
 
 func main() {
-	prompt := promptui.Select{
-		Label: "Select Day",
-		Items: []string{"Monday", "Tuesday", "Wednesday", "Thursday", "Friday",
-			"Saturday", "Sunday"},
+	days := []string{}
+	prompt := &survey.MultiSelect{
+		Message: "What days do you prefer:",
+		Options: []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"},
 	}
-
-	_, result, err := prompt.Run()
-
-	if err != nil {
-		fmt.Printf("Prompt failed %v\n", err)
-		return
-	}
-
-	fmt.Printf("You choose %q\n", result)
+	survey.AskOne(prompt, &days)
 }
